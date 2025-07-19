1064e2f8773cfaed09e37841e24ec77e403b4a30
Berkan Türkali <berkanturkali@outlook.com>
2025-07-19 08:57:52 +0300

    add nunito font

---
+++ obilet-ios-assignment/Utils/Nunito.swift
@@ -0,0 +1,72 @@
+//
+
+import Foundation
+
+struct Nunito {
+    
+    
+    static var black: String {
+        "Nunito-Black"
+    }
+    
+    static var blackItalic: String {
+        "Nunito-BlackItalic"
+    }
+    
+    static var bold: String {
+        "Nunito-Bold"
+    }
+    
+    static var boldItalic: String {
+        "Nunito-BoldItalic"
+    }
+    
+    static var extraBold: String {
+        "Nunito-ExtraBold"
+    }
+    
+    static var extraBoldItalic: String {
+        "Nunito-ExtraBoldItalic"
+    }
+    
+    static var extraLight: String {
+        "Nunito-ExtraLight"
+    }
+    
+    static var extraLightItalic: String {
+        "Nunito-ExtraLightItalic"
+    }
+    
+    static var italic: String {
+        "Nunito-Italic"
+    }
+    
+    static var light: String {
+        "Nunito-Light"
+    }
+    
+    static var lightItalic: String {
+        "Nunito-LightItalic"
+    }
+    
+    static var regular: String {
+        "Nunito-Regular"
+    }
+    
+    static var medium: String {
+        "Nunito-Medium"
+    }
+    
+    static var mediumItalic: String {
+        "Nunito-MediumItalic"
+    }
+    
+    static var semiBold: String {
+        "Nunito-SemiBold"
+    }
+    
+    static var semiBoldItalic: String {
+        "Nunito-SemiBoldItalic"
+    }
+    
+}
